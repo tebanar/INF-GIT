48263f576450efb963a92178b7fd71ade7a94b69
tebanar <146668270+tebanar@users.noreply.github.com>
2023-10-01 21:41:26 +0200

    Add files via upload

---
+++ while.py
@@ -0,0 +1,31 @@
+# suma = 0
+# while True:
+#     inp = input()
+#     if inp == "stop":
+#         break
+#     inp = float(inp)
+#     suma += inp
+# print(suma)
+
+# suma = 0
+# inp = int(input())
+# while inp >= 0:
+#     if inp % 7 == 0:
+#         suma += inp
+#     inp -= 1
+# print(suma)
+
+
+# suma = 0
+# inp = int(input())
+# while inp >= 0:
+#     if inp % 7 == 0 and inp %14 != 0:
+#         suma += inp
+#     inp -= 1
+# print(suma)
+
+n = int(input())
+while n >= 0:
+    print(2**n)
+    n -= 1
+
